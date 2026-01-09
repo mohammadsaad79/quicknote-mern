d34f35ee1da2f21792ca8c413d0ac447d8d45675
Mohammad Saad <saadayinas9322@gmail.com>
2026-01-10 03:43:00 +0530

    Fix reset password route refresh issue

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -5,15 +5,16 @@ const sendEmail = async (to, subject, html) => {
     service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
+      pass: process.env.EMAIL_PASS,
+    },
+    connectionTimeout: 10000,
   });
 
   await transporter.sendMail({
-    from: `"Notes App" <${process.env.EMAIL_USER}>`,
+    from: `"QuickNote" <${process.env.EMAIL_USER}>`,
     to,
     subject,
-    html
+    html,
   });
 };
 
